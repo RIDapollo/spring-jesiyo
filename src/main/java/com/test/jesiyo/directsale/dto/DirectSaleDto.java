41b892b0fe284bfced8780c8ccfa4f96e117f064
JJanE2 <aaa1685@naver.com>
2026-04-23 23:42:34 +0900

    feat: add trade_location table and trade_location dto

--- src/main/java/com/test/jesiyo/directsale/dto/DirectSaleDto.java
+++ src/main/java/com/test/jesiyo/directsale/dto/DirectSaleDto.java
@@ -9,4 +9,13 @@ import lombok.Data;
 public class DirectSaleDto {
 	private Long seq;
 	private String name;
+	private String description;
+	private String status;
+	private String productName;
+	private String imageUrl;
+	private Long price;
+	private String createdAt;
+	private Long sellerSeq;
+	private Long categorySeq;
+	private Long tradeLocationSeq;
 }
