cc1320e07e274850f4a4e09d9f5b46b101b8f2bc
wauder0430 <wlaudaoq123@naver.com>
2026-04-23 10:46:49 +0900

    feat: 임시 맴버 xml 생성

--- dbscripts/DML-박지명.sql
+++ dbscripts/DML-박지명.sql
@@ -1,7 +1,15 @@
 select * from member;
 
+-- 회원 더미
 INSERT INTO MEMBER (SEQ, NAME, POINT, ID, PW, ADDRESS, BIRTH, NICKNAME, PERMISSION, REGDATE, PW_TOKEN, TOKEN_EXPIRY, STATUS, EMAIL_ADDRESS, LOCATION_SEQ) VALUES (member_seq.nextval, '엄준식', default, 'um1234', 'java1234', '서울시 강남구 대치동', '2000-02-03', '어떻게사람이름이엄준식', 0, DEFAULT, null, null, DEFAULT, 'um1234@test.com', null);
 
 INSERT INTO MEMBER (SEQ, NAME, POINT, ID, PW, ADDRESS, BIRTH, NICKNAME, PERMISSION, REGDATE, PW_TOKEN, TOKEN_EXPIRY, STATUS, EMAIL_ADDRESS, LOCATION_SEQ) VALUES (member_seq.nextval, '윤진석', default, 'yun1234', 'java1234', '서울시 강남구 역삼동', '1995-04-14', '윤가놈', 0, DEFAULT, null, null, DEFAULT, 'yun1234@test.com', null);
 
 commit;
+
+SELECT * from CATEGORY;
+
+-- 채팅방 더미
+INSERT INTO CHAT_ROOM (SEQ, TITLE, STATUS, MAX_MEMBER_CNT, CURRENT_MEMBER_CNT, CATEGORY_SEQ, MEMBER_SEQ, CODE) VALUES (CHAT_ROOM_SEQ.nextval, '윤진석의 전자기기 채팅방', DEFAULT, 50, default, 1, 4, 'aI32IKdw');
+
+select * from CHAT_ROOM;
