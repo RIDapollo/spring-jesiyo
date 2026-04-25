f8666f3a891f74f8eae417c43cb1fc0641c969a3
kmd192kmd <kmd192kmd@gmail.com>
2026-04-25 14:57:28 +0900

    bidWebSocketHandler 구현 및 servlet-context에 설정추가

---
+++ src/main/java/com/test/jesiyo/auction/handler/BidWebSocketHandler.java
@@ -0,0 +1,35 @@
+package com.test.jesiyo.auction.handler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class BidWebSocketHandler extends TextWebSocketHandler {
+
+	private static List<WebSocketSession> sessions = new ArrayList<>();
+	
+	@Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session); 
+    }
+	
+	@Override
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		
+		for (WebSocketSession s : sessions) {
+			s.sendMessage(new TextMessage(message.getPayload()));
+		}
+	}
+	
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		sessions.remove(session);
+	}
+	
+}
