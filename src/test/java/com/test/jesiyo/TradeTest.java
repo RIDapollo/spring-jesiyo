fea26d4dd7bc27ba41b4ff32a82318a22c4ec107
JJanE2 <aaa1685@naver.com>
2026-04-27 10:21:14 +0900

    feat: add trade create API

---
+++ src/test/java/com/test/jesiyo/TradeTest.java
@@ -0,0 +1,33 @@
+package com.test.jesiyo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.test.jesiyo.trade.dto.TradeDto;
+import com.test.jesiyo.trade.service.TradeService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"file:src/main/webapp/WEB-INF/spring/root-context.xml", "file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"})
+public class TradeTest {
+
+	@Autowired
+	TradeService service;
+	
+	@Test
+	public void testTradeAdd() {
+		// given
+		TradeDto tradeDto = new TradeDto();
+		tradeDto.setDirectSaleSeq(8L);
+		tradeDto.setSellerSeq(1L);
+		tradeDto.setBuyerSeq(2L);
+
+		// when
+		int result = service.add(tradeDto);
+
+		// then
+		System.out.println(result);
+	}
+}
