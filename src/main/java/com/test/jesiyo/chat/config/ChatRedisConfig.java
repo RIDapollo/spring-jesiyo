0f9bfd12fb1714dcd3c0537573002c428b1df1a7
wauder0430 <wlaudaoq123@naver.com>
2026-04-28 23:22:05 +0900

    fix: chat-handler Redis

--- src/main/java/com/test/jesiyo/chat/config/ChatRedisConfig.java
+++ src/main/java/com/test/jesiyo/chat/config/ChatRedisConfig.java
@@ -1,5 +1,6 @@
 package com.test.jesiyo.chat.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -14,12 +15,12 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import com.test.jesiyo.chat.redis.RedisSubscriber;
 
 @Configuration
-public class RedisConfig {
+public class ChatRedisConfig {
 
     // Redis 서버(host, port) 정보로 연결 팩토리 생성
     // LettuceConnectionFactory가 실제 Redis 연결을 담당
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
+	@Bean(name = "chatRedisConnectionFactory")
+    public RedisConnectionFactory chatRedisConnectionFactory() {
         RedisStandaloneConfiguration config =
                 new RedisStandaloneConfiguration("127.0.0.1", 6379);
         return new LettuceConnectionFactory(config);
@@ -28,8 +29,8 @@ public class RedisConfig {
     // Redis에 값을 읽고 쓰기 위한 템플릿
     // 지금은 JSON 문자열을 그대로 publish/subscribe 할 예정이므로
     // key, value serializer를 모두 String 방식으로 설정
-    @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+	@Bean(name = "chatRedisTemplate")
+    public RedisTemplate<String, String> chatRedisTemplate(@Qualifier("chatRedisConnectionFactory") RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
 
@@ -51,21 +52,21 @@ public class RedisConfig {
 
     // Redis Pub/Sub에서 사용할 채널 이름
     // 처음에는 고정 채널 하나("chat")로 시작
-    @Bean
+	@Bean(name = "chatTopic")
     public ChannelTopic chatTopic() {
         return new ChannelTopic("chat");
     }
 
     // Redis에서 메시지를 받았을 때 실행할 subscriber 연결
     // RedisSubscriber의 onMessage 메서드를 호출하도록 설정
-    @Bean
+	@Bean(name = "chatRedisListenerAdapter")
     public MessageListenerAdapter messageListenerAdapter(RedisSubscriber redisSubscriber) {
         return new MessageListenerAdapter(redisSubscriber, "onMessage");
     }
 
     // Redis Pub/Sub 메시지를 실제로 수신하는 컨테이너
     // 어떤 listener가 어떤 topic을 구독할지 등록
-    @Bean
+	@Bean(name = "chatRedisMessageListenerContainer")
     public RedisMessageListenerContainer redisMessageListenerContainer(
             RedisConnectionFactory connectionFactory,
             MessageListenerAdapter listenerAdapter,
